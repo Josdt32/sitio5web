599d1881a297aad56d044b8e99f9a99e22f2ed1b
Josdt32 <168057053+Josdt32@users.noreply.github.com>
2024-04-25 06:42:17 -0700

    Add files via upload

---
+++ queso.js
@@ -0,0 +1,11 @@
+// Interacción con el usuario //
+alert("¡Bienvenido a Quesería Toledo!");
+
+// Imágenes y audio (ejemplo) //
+
+
+const img = document.createElement("img");
+img.src = "Queso.jpg";
+document.body.appendChild(img);
+
+
